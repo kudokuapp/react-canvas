cb8317841b027aa25e3dfb4932b0dd25725b5c2d
Furqon Wilogo <fdwilogo@gmail.com>
2022-06-17 21:34:57 +0700

    commit

---
+++ .storybook/KudokuTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Kudoku React Component',
+  brandUrl: 'https://kudoku.id',
+  brandImage: 'https://drive.google.com/uc?id=1pJH21GQufDoJu88E4TfrPu5GyjAh3X-l',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import KudokuTheme from './KudokuTheme';
+
+addons.setConfig({
+  theme: KudokuTheme,
+});
