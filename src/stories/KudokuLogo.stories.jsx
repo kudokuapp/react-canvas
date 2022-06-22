6dc273fdd1f0e071683f1f748dd63452a6424b55
Furqon Wilogo <69423972+fdwilogo@users.noreply.github.com>
2022-06-22 13:43:47 +0700

    commit

--- src/stories/KudokuLogo.stories.jsx
+++ src/stories/KudokuLogo.stories.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import { withDesign } from "storybook-addon-designs";
 
-import { KudokuLogo } from '../primitives/KudokuLogo';
+import { KudokuLogo } from "../primitives/KudokuLogo";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Primitives/Kudoku Logo',
+  title: "Primitives/Kudoku Logo",
   component: KudokuLogo,
   decorators: [withDesign],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     variant: {
-      options: ['primary', 'secondary', 'variant1', 'variant2'],
-      control: { type: 'radio' },
-    },
-  },
+      options: ["primary", "secondary", "variant1", "variant2"],
+      control: { type: "radio" }
+    }
+  }
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -23,28 +23,28 @@ const Template = (args) => <KudokuLogo {...args} />;
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = Template.bind({});
 Primary.args = {
-  variant: 'primary',
+  variant: "primary"
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  variant: 'secondary',
+  variant: "secondary"
 };
 
 export const Variant1 = Template.bind({});
 Variant1.args = {
-  variant: 'variant1',
+  variant: "variant1"
 };
 
 export const Variant2 = Template.bind({});
 Variant2.args = {
-  variant: 'variant2',
+  variant: "variant2"
 };
-
 
 Primary.parameters = {
   design: {
     type: "figma",
-    url: "https://www.figma.com/file/n6Sqzg6fE0JIBbAPys2r9X/Primitives?node-id=333%3A196",
-  },
+    url:
+      "https://www.figma.com/file/n6Sqzg6fE0JIBbAPys2r9X/Primitives?node-id=333%3A196"
+  }
 };
